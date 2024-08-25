453d62ed72efa6e86748e38db5b77740d9bc019b
Luquetti <gustavo.luquetti@hotmail.com>
2024-08-25 20:20:29 -0300

    termina validação

--- domain/Validacoes/ValidaDrinks.cs
+++ domain/Validacoes/ValidaDrinks.cs
@@ -10,6 +10,26 @@ namespace domain.Validacoes
         public ValidaDrinks(IDrinkRepositorio drinkRepositorio)
         {
             _drinkRepositorio = drinkRepositorio;
+            CascadeMode cascade = CascadeMode.Stop;
+            RuleFor(Drinks => Drinks.Nome)
+                .NotNull()
+                .WithMessage("Qual nome da bebida?")
+                .NotEmpty()
+                .WithMessage("Qual nome da bebida?");
+
+            RuleFor(Drinks => Drinks.Preco)
+            .NotNull()
+            .WithMessage("Qual o valor da mercadoria?")
+            .NotEmpty()
+            .WithMessage("Qual o valor da mercadoria ?");
+
+            RuleFor(Drinks => Drinks.Composicao)
+            .IsInEnum()
+            .WithMessage("Não existe")
+            .NotEmpty()
+            .WithMessage("Não existe drink sem composição");
+
+
         }
     }
 }
